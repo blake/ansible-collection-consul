a5a3bea086e261b5d342254b4d91df999464bd2b
Blake Covarrubias <blake@covarrubi.as>
2022-03-19 12:57:24 -0700

    Make consul-redirect-traffic support multi-app deploy

--- roles/transparent_proxy/files/consul-redirect-traffic.sh
+++ roles/transparent_proxy/files/consul-redirect-traffic.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # This is a small wrapper around `consul connect redirect-traffic` which
-# Determines the user ID for the `consul` process and `envoy` sidecar
-# prior to executing the command to install the redirect rules.
+# determines the user ID for the `envoy` process prior to executing the command
+# to install the redirect rules so that it can be exempted from traffic
+# redirection.
 #
 # Syntax is: consul-redirect-traffic <service name>
 
@@ -19,17 +20,10 @@ if [[ $# -eq 0 ]]; then
     usage
 fi
 
-# Obtain user IDs for consul and envoy
-CONSUL_UID=$(id --user consul)
+SERVICE_NAME=$1
+
+# Obtain the user ID for envoy
 PROXY_UID=$(id --user envoy)
-
-# Redirect DNS traffic to Consul using iptables if systemd version < 246
-SYSTEMD_VERSION=$(systemd --version | awk 'NR==1 { print $2 }')
-
-if [[ ${SYSTEMD_VERSION} -lt 246 ]]; then
-    iptables --table nat --append OUTPUT --destination localhost --proto udp --match udp --dport 53 --jump REDIRECT --to-ports 8600
-    iptables --table nat --append OUTPUT --destination localhost --proto tcp --match tcp --dport 53 --jump REDIRECT --to-ports 8600
-fi
 
 if [[ -f "/etc/consul.d/service-registration.json" ]]; then
   CONFIGURED_PROXY_MODE=$(jq --raw-output .service.connect.sidecar_service.proxy.mode /etc/consul.d/service-registration.json)
@@ -43,12 +37,11 @@ if [[ -f "/etc/consul.d/service-registration.json" ]]; then
 fi
 
 # Include extra redirection exemptions if present
-if [[ -f "/srv/consul/extra-args.json" ]]; then
-  EXTRA_REDIRECT_ARGS=$(jq --raw-output .redirect /srv/consul/extra-args.json)
+if [[ -f "/srv/consul/services/${SERVICE_NAME}/extra-args.json" ]]; then
+  EXTRA_REDIRECT_ARGS=$(jq --raw-output .redirect "/srv/consul/services/${SERVICE_NAME}/extra-args.json")
 fi
 
 eval consul connect redirect-traffic \
-    -proxy-id="${1}-sidecar-proxy" \
+    -proxy-id="${SERVICE_NAME}-sidecar-proxy" \
     -proxy-uid="${PROXY_UID}" \
-    -exclude-uid="${CONSUL_UID}" \
     "${EXTRA_REDIRECT_ARGS}"
